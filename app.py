e20ba26465277700e9afb2ecb08df944a3f55649
Joset2804 <joset2804@hotmail.com>
2024-06-13 19:15:02 -0500

    [FIX]: CORS Configuration

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 
 # Cargar el modelo
 model = joblib.load("model.pkl")
